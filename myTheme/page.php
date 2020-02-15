245d1538288998b15e3537dac7a067c014e0440c
s1hagog <moshakalex@gmail.com>
2020-02-14 21:13:09 -0800

    singular and page php files

---
+++ myTheme/page.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<h1>This is the page.php file</h1>
+
+<?php get_footer(); ?>
---
+++ myTheme/singular.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+<h1>This is the singular.php file</h1>
+
+<?php get_footer(); ?>
