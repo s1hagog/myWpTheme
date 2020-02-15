34236227b0d900ff78bfc09cbf8aed1fae0e108d
s1hagog <moshakalex@gmail.com>
2020-02-14 21:04:30 -0800

    created new post type - projects

--- myTheme/functions.php
+++ myTheme/functions.php
@@ -14,12 +14,34 @@ add_action('wp_enqueue_scripts', 'mytheme_setup');
 
 function mytheme_init() {
     add_theme_support('post-thumbnails');
-    add_theme_suppot('title-tag');
+    add_theme_support('title-tag');
     add_theme_support( 'html5', 
         array('comment-list', 'comment-form', 'search-form') 
     );
 }
 
+add_action( 'after_setup_theme', 'mytheme_init');
+
 //Project Post Type 
 
-function mytheme_custom_post_type() {}
+function mytheme_custom_post_type() {
+    register_post_type('project',
+        array(
+            'rewrite' => array('slug' => 'projects'),
+            'labels' => array(
+                'name' => 'Projects',
+                'singular_name' => 'Project',
+                'add_new_item' => 'Add New Project',
+                'edit_item' => 'Edit Project'
+            ),
+            'menu-icon' => 'dashicons-clipboard',
+            'public' => true,
+            'has_archive' => true,
+            'supports' => array(
+                'title', 'thumbnail', 'editor', 'excerpt', 'comments'
+            )
+        )
+    );
+}
+
+add_action( 'init', 'mytheme_custom_post_type');
